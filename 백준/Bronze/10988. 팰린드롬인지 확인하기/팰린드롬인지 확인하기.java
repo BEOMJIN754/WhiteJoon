5e21d0231ae80ae0738263250633ccf8b8f4373e
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2024-04-25 17:06:51 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 204 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -7,12 +7,12 @@ public class Main {
         char[] compare = input.toCharArray();
         int result = 1;
 
-        for(int i=0;i<compare.length;i++){
+        for(int i=0;i<compare.length /2;i++){ // 얘 반절 자르기
                 if(compare[i] !=compare[compare.length-1-i]){
                     result = 0;
+                    break;
                 }
             }
         System.out.println(result);
         }
     }
-
