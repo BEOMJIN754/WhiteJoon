bdeefe2480ae4963de36b18a959f89df985c223a
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2024-07-24 11:33:49 +0900

    [Silver V] Title: 라디오, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/3135. 라디오/라디오.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 입력 부분
+        String[] line = br.readLine().split(" ");
+        int A = Integer.parseInt(line[0]);
+        int B = Integer.parseInt(line[1]);
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] favorites = new int[N];
+        for (int i = 0; i < N; i++) {
+            favorites[i] = Integer.parseInt(br.readLine());
+        }
+        
+        System.out.println(findMinButtonPresses(A, B, favorites));
+    }
+    
+    public static int findMinButtonPresses(int A, int B, int[] favorites) {
+        // 초기 거리: A에서 B까지 직접 이동
+        int minPresses = Math.abs(A - B);
+        
+        // 즐겨찾기 채널을 이용해 계산
+        for (int fav : favorites) {
+            int presses = 1 + Math.abs(fav - B);
+            if (presses < minPresses) {
+                minPresses = presses;
+            }
+        }
+        
+        return minPresses;
+    }
+}
