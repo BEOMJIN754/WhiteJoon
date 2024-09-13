c91d45b50c5d1435b619024a16549de4bb4e56b4
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2024-09-13 13:41:47 +0900

    [Bronze I] Title: 평균, Time: 204 ms, Memory: 18640 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -2,28 +2,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int A[] = new int[N];
+		for(int i=0;i<N;i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		long max =0;
+		long sum =0;
+		
+		for(int i=0;i<N;i++) {
+			if(A[i]>max) {
+				max=A[i];
+			}
+			sum=sum+A[i];
+		}
+		System.out.println(sum*100.0/max/N);
+ 	}
 
-        int n = sc.nextInt(); // 입력받을 숫자의 개수
-        int[] arr = new int[n];
-        long max = 0;  // 최댓값을 저장할 변수
-        double sum = 0;  // 합계를 저장할 변수
-
-        // 입력값을 배열에 저장하면서 최댓값 찾기
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-
-        // 각 숫자를 최댓값으로 나누고 100을 곱해 합산
-        for (int i = 0; i < n; i++) {
-            sum += (double) arr[i] / max * 100;  // 실수 나눗셈을 위해 (double)로 캐스팅
-        }
-
-        // 평균값 출력
-        System.out.println(sum / n);
-    }
 }
