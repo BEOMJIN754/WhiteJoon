c8d5c14e19c126d43d5785ee4f36e7a3abd3bfea
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2024-11-07 15:39:58 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,28 +1,24 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String number = br.readLine().trim();  // 입력받은 숫자를 문자열로 처리
-
-        // 각 자리수를 문자열 배열로 변환
-        String[] digits = number.split("");
         
-        // 내림차순 정렬
-        Arrays.sort(digits, Collections.reverseOrder());
+        String num = br.readLine().trim();
         
-        // 정렬된 배열을 다시 하나의 문자열로 합침
-        StringBuilder sortedNumber = new StringBuilder();
-        for (String digit : digits) {
-            sortedNumber.append(digit);
+        String[] arr = num.split("");
+        
+        Arrays.sort(arr,Collections.reverseOrder());
+        
+        for(int i=0;i<arr.length;i++) {
+        	System.out.print(arr[i]);
         }
-
-        // 결과 출력
-        System.out.println(sortedNumber.toString());
+        
     }
 }
 
