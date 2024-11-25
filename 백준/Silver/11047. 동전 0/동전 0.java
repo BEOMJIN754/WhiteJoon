0d2ab3cbb9f59a3dfa82b4cc2e48c91a420f6e85
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2024-11-25 13:47:55 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,25 +1,28 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int A[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
 
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int K = sc.nextInt();
-		int A[] = new int[N];
-		for (int i = 0; i < N; i++) {
-			A[i] = sc.nextInt();
-		}
-
-		int count = 0;
-		for (int i = N - 1; i >= 0; i--) {
-			if (A[i] <= K) {
-				count += (K / A[i]);
-				K = K % A[i];
-			}
-		}
-		System.out.println(count);
-	}
+        int count = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (A[i] <= K) {
+                count += (K / A[i]);
+                K = K % A[i];
+            }
+        }
+        System.out.println(count);
+    }
 }
