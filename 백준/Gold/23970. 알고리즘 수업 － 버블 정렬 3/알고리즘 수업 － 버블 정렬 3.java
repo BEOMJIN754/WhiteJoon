7a54337ebe15c161864b695a49c484a2f79edd38
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2025-01-08 23:02:40 +0900

    [Gold IV] Title: 알고리즘 수업 - 버블 정렬 3, Time: 428 ms, Memory: 19416 KB -BaekjoonHub

--- 백준/Gold/23970. 알고리즘 수업 － 버블 정렬 3/알고리즘 수업 － 버블 정렬 3.java
+++ 백준/Gold/23970. 알고리즘 수업 － 버블 정렬 3/알고리즘 수업 － 버블 정렬 3.java
@@ -1,42 +1,47 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String[] tmp = br.readLine().split(" ");
-        int[] a = Arrays.stream(tmp).mapToInt(Integer::parseInt).toArray();
-        tmp = br.readLine().split(" ");
-        int[] b = Arrays.stream(tmp).mapToInt(Integer::parseInt).toArray();
 
-        if (Arrays.equals(a, b)) {
+        int num = Integer.parseInt(br.readLine());
+        int[] A = new int[num];
+        int[] B = new int[num];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < num; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringTokenizer stt = new StringTokenizer(br.readLine());
+        for (int i = 0; i < num; i++) {
+            B[i] = Integer.parseInt(stt.nextToken());
+        }
+        
+        if (Arrays.equals(A, B)) {
             System.out.println(1);
             return;
         }
-
-        // boolean flag;
-        for (int i = n - 1; i >= 1; i--) {
-            // flag = true;
-            for (int j = 0; j < i; j++) {
-                if (a[j] > a[j + 1]) {
-                    // flag = false;
-                    int t = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = t;
-                    if (a[j] == b[j] && a[j + 1] == b[j + 1] && Arrays.equals(a, b)) {
-                        System.out.println(1);
-                        return;
+        
+        for (int i = 0; i < num - 1; i++) {
+            for (int j = 0; j < num - 1 - i; j++) {  // 버블 정렬
+                if (A[j] > A[j + 1]) {
+                    int temp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
+                    
+                    if(A[j]==B[j]&&A[j+1]==B[j+1]&&Arrays.equals(A, B)) {
+                    	System.out.println(1);
+                    	return;
                     }
                 }
             }
 
-            // if (flag) {
-            //     break;
-            // }
         }
-
-        System.out.println(0);
+            System.out.println(0);
     }
 }
