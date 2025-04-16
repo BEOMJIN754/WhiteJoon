dc4c13b79454eaed8c93b9bb50d04ab8a77700b7
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2025-04-16 17:13:32 +0900

    [Bronze V] Title: 사분면 고르기, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -2,43 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int input1 = scan.nextInt();
-        int input2 = scan.nextInt();
-
-        int state = getState(input1, input2);
-
-
-        switch (state) {
-            case 1:
-                System.out.println(1);
-                break;
-            case 2:
-                System.out.println(2);
-                break;
-            case 3:
-                System.out.println(3);
-                break;
-            case 4:
-                System.out.println(4);
-                break;
-        }
-        scan.close();
-    }
-
-    public static int getState(int input1, int input2) {
-        if (input1 > 0 && input2 > 0) {
-            return 1;
-        } else if (input1 < 0 && input2 > 0) {
-            return 2;
-        } else if (input1 < 0 && input2 < 0) {
-            return 3;
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if(x>0){
+            if(y>0) System.out.println(1);
+            else System.out.println(4);
         } else {
-            return 4;
+            if (y>0) System.out.println(2);
+            else System.out.println(3);
         }
-
+        sc.close();
     }
-
 }
-
