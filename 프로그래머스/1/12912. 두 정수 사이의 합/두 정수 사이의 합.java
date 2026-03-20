01ab9c206f4c881cdd0a08aed4ba68eaf7a4b9bb
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2026-03-20 17:05:19 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 13.01 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public long solution(int a, int b) {
+        
+        long answer =0;
+        
+        if(a==b)return a;
+        int min, max;
+        min = Math.min(a,b);
+        max = Math.max(a,b);
+        
+        for(int i=min; i<=max;i++){
+            answer +=i;
+        }
+        return answer;
+    }
+}
