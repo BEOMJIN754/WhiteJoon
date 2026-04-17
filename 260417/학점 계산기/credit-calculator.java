cbbd4e84d3f3975b1ee112e776ca111553b9f853
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2026-04-17 20:15:02 +0900

    Title: 학점 계산기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260417/학점 계산기/credit-calculator.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     StringTokenizer st = new StringTokenizer(br.readLine());
+
+     int n = Integer.parseInt(st.nextToken());
+     double g = 0.0;
+
+    st = new StringTokenizer(br.readLine());
+     for(int i=0;i<n;i++){
+        g+=Double.parseDouble(st.nextToken());
+     }
+    double avg = g/n;
+    System.out.printf("%.1f%n",avg);
+    if(avg>4.0) System.out.println("Perfect");
+    else if(avg>3.0) System.out.println("Good");
+    else System.out.println("Perfect");
+
+
+    }
+}
