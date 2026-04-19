0ac9e9b9e8bfa7313621d537afeb67df484a0cab
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2026-04-19 17:52:19 +0900

    Title: 10개 입력받기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260419/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int cnt =0;
+        int sum =0;
+        int[] arr = new int[10];
+
+        for(int i=0;i<10;i++){
+            int a = Integer.parseInt(st.nextToken());
+            if(a==0) break;
+            else{}
+            arr[i] = a;
+            sum+=a;
+            cnt++;}
+            
+            System.out.print(sum+" ");
+            System.out.printf("%.1f%n",(double)sum/cnt);
+            
+        }
+
+
+
+}
