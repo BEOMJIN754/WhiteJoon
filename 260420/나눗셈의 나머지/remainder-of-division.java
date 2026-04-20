9c12a9a610493e6c03e3cf289fe2840cd0059118
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2026-04-20 17:12:32 +0900

    Title: 나눗셈의 나머지, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260420/나눗셈의 나머지/remainder-of-division.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    
+        int t = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        
+        int[] cArr = new int[d];
+
+        while(t>0){
+            cArr[t%d]++;
+            t = t/d;
+        }
+
+        int sum =0;
+        for(int i=0;i<cArr.length;i++){
+            sum += cArr[i]*cArr[i];
+        }
+        System.out.println(sum);
+    }
+}
