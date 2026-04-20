9807f09d761b781ef668c555f19972accf2ac9f1
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2026-04-20 15:56:31 +0900

    Title: 십의 자리 숫자, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260420/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num = st.countTokens();
+        int[] arr = new int[10];
+
+        for(int i=0; i<num; i++){
+            int t = Integer.parseInt(st.nextToken());
+            arr[t/10]++;
+        }
+        
+        for(int i =1;i<10;i++){
+            System.out.println(i+" - "+arr[i]);
+        }
+    }
+}
