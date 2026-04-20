3b52425db68383d45df48ed7a5796b0f4c990a6a
BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
2026-04-20 16:37:55 +0900

    Title: 코로나 메뉴얼2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260420/코로나 메뉴얼2/covid-manual2.java
+++ 260420/코로나 메뉴얼2/covid-manual2.java
@@ -1,25 +1,26 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-         StringTokenizer st;
         int[] cArr = new int[4];
 
-        for(int i=0; i<3;i++){
-         st = new StringTokenizer(br.readLine());
-          if(st.nextToken().equals("Y")){
-            if(Integer.parseInt(st.nextToken())>=37) cArr[0]++;
-            else cArr[2]++; 
-          }
-          else{
-            if(Integer.parseInt(st.nextToken())>=37) cArr[1]++;
-            else cArr[3]++;
-          }
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            boolean isY = st.nextToken().equals("Y");
+            boolean isHigh = Integer.parseInt(st.nextToken()) >= 37;
+
+            if (isY && isHigh)       cArr[0]++;
+            else if (!isY && isHigh) cArr[1]++;
+            else if (isY)            cArr[2]++;
+            else                     cArr[3]++;
         }
-        for(int i=0; i<4;i++){
-            System.out.print(cArr[i]+" ");
-        }
-        if(cArr[0]>=2) System.out.print("E");
+
+        StringBuilder sb = new StringBuilder();
+        for (int c : cArr) sb.append(c).append(" ");
+        if (cArr[0] >= 2) sb.append("E");
+
+        System.out.println(sb.toString().trim());
     }
 }
